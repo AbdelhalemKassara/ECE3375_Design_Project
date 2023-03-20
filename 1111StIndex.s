b7a84fdf801e8ab4e51e0c3d9a8a0af9798e937e
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2023-03-20 19:24:02 -0400

    clear registers

--- 1111StIndex.s
+++ 1111StIndex.s
@@ -18,6 +18,20 @@
 .global _start
 _start:
 mov r0, #1
+@clear the registers
+mov r1, #0
+mov r2, #0
+mov r3, #0
+mov r4, #0
+mov r5, #0
+mov r6, #0
+mov r7, #0
+mov r8, #0
+mov r9, #0
+mov r10, #0
+mov r11, #0
+mov r12, #0
+
 loop:
   bl UPDATE_TARGET_TEMP
   bl SET_SINGLE_SEG
